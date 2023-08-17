1a7de24fb4ca03126ee2e2a3b59064fcccf385e9
Yusuf CEYLAN <yusuf.ceylann@outlook.com.tr>
2023-08-17 16:42:14 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String değer, sayı giriniz (Örnek giriş formatı: Algoritma,3 Algoritma,5 Algoritma,22 Algoritma,0): ");
+            string inputValue = Console.ReadLine();
+
+            string[] stringArray = inputValue.Split(',', ' ');
+            string output = "";
+
+            for (int i = 0; i < stringArray.Length; i += 2)
+            {
+                output += RemoveChartAtIndex(stringArray[i], Convert.ToInt32(stringArray[i + 1])) + " ";
+            }
+
+            Console.WriteLine(output);
+
+        }
+
+        public static string RemoveChartAtIndex(string str, int index)
+        {
+            return str.Remove(index, 1);
+        }
+    }
+}
